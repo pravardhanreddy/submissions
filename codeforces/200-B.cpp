9abc37a5abe7e044acdc79bf26f2928a410a5a7f
Pravardhan <pravardhan.green1998@gmail.com>
2021-06-04 13:08:09 +0530

    new

---
+++ codeforces/200-B.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0, _##i = (n); i < _##i; ++i)
+#define REP(i,k,n) for (i = k; i <= n; ++i)
+typedef long long ll;
+using namespace std;
+
+void code_init () {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main()
+{
+    code_init();
+
+    int n;
+    cin >> n;
+    double a, count = 0;
+    rep(i,n){
+        cin >> a;
+        count += a/n;
+    }
+    cout << count;
+    return 0;
+}
