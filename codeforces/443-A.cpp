eb56c1661796fc18cd369cc486aab3a18176cf19
Pravardhan <pravardhan.green1998@gmail.com>
2021-06-06 22:52:59 +0530

    new

---
+++ codeforces/443-A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0, _##i = (n); i < _##i; ++i)
+#define REP(i,k,n) for (int i = k, _##i = (n); i < _##i; ++i)
+typedef long long ll;
+using namespace std;
+
+void code_init () {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main()
+{
+    code_init();
+
+    string s;
+    getline(cin,s);
+    set<char> v;
+
+    rep(i, s.length()){
+        if(s[i] >= 'a' && s[i] <= 'z') v.insert(s[i]);
+    }
+    
+    cout << v.size();
+    return 0;
+}
