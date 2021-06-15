07bb296242d9b038ac96b6f0e9c9ff91e98e5a45
Pravardhan <pravardhan.green1998@gmail.com>
2021-06-15 21:10:34 +0530

    new

---
+++ codeforces/581-A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+#define rep(i, n) for (int i = 0, _##i = (n); i < _##i; ++i)
+#define REP(i,k,n) for (int i = k, _##i = (n); i < _##i; ++i)
+typedef long long ll;
+using namespace std;
+
+void code_init () {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main()
+{
+    code_init();
+
+    int a, b;
+    cin >> a >> b;
+    cout << min(a,b) << " " << abs(a-b)/2;
+    
+    return 0;
+}
