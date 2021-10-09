18353e09e0e7684f7defefbbc8dbabb9972dd98a
pravardhanreddy <pravardhan.green1998@gmail.com>
2021-10-09 18:34:22 -0400

    new

---
+++ leetcode/941. Valid Mountain Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+        
+        int i = 0, j = arr.length - 1;
+        
+        while(i<j && arr[i] < arr[i+1]) i++;
+        while(j>i && arr[j] < arr[j-1]) j--;
+        
+        return (i==j && i>0 && j<arr.length-1);
+    }
+}
